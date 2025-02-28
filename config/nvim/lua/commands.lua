5f4492bb3a71b12eb478e35e853bb94a204c379d
obrusvit <vit.obrusnik@gmail.com>
2025-02-28 13:04:43 +0100

    chore(nvim): remove obsolete cmd

--- config/nvim/lua/commands.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_create_user_command("Mkses", function(args)
-  local session_name = args[1] or "ses_nvim"
-  vim.cmd("mksession!" .. session_name)
-end, { nargs = "?", desc = "Make session" })
