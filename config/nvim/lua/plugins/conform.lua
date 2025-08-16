798986d724e16a6a69219f09340312f59c199b93
obrusvit <vit.obrusnik@gmail.com>
2025-08-16 23:09:21 +0200

    chore(nvim): use prettier
    - also lazy-lock housekeeping

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -24,7 +24,7 @@ return {
       -- Conform can also run multiple formatters sequentially
       python = { "isort", "black" },
       rust = { "rustfmt" },
-      json = { "jq" },
+      json = { "prettier" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
