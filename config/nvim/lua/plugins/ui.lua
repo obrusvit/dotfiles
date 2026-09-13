9183dd2697871c4fa9902edfc6aec643c12bc1c7
obrusvit <vit.obrusnik@gmail.com>
2026-09-13 21:38:06 +0200

    feat(which-key): add <leader>g Git group

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -73,6 +73,7 @@ return {
       spec = {
         { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
         { "<leader>d", group = "[D]ocument/[D]ebug" },
+        { "<leader>g", group = "[G]it" },
         { "<leader>r", group = "[R]ename" },
         { "<leader>s", group = "[S]earch" },
         { "<leader>w", group = "[W]orkspace" },
